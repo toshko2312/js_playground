a83bfb79ae010de54d7e09197b17ba19a65cd35e
Todor Milushev <toshko_2312@abv.bg>
2024-01-26 17:43:07 +0200

    Add bubble_sort

---
+++ bubble_sort/bubble_sort.js
@@ -0,0 +1,15 @@
+const bubbleSort = (numbers) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length - (i + 1); j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        const temp = numbers[j + 1];
+        numbers[j + 1] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
+  }
+  return numbers;
+};
+
+console.log(bubbleSort([3, 6, 1, 3]));
+console.log(bubbleSort([]));
