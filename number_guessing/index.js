daf92694e6acd98a314a90d6fb866bf3062da794
Todor Milushev <toshko_2312@abv.bg>
2024-01-27 19:25:15 +0200

    Update guessing game

--- number_guessing/index.js
+++ number_guessing/index.js
@@ -7,6 +7,7 @@ const answer = Math.floor(
 document.getElementById("btn").onclick = () => {
   const input = document.getElementById("numberInput");
   const result = document.getElementById("result");
+  const image = document.getElementById("resultImage");
 
   if (isNaN(+input.value) || input.value == "") {
     result.textContent = `Please enter a valid number!`;
@@ -15,12 +16,18 @@ document.getElementById("btn").onclick = () => {
     location.reload();
   } else if (input.value > answer) {
     result.textContent = `Try guessing lower! (${input.value})`;
+    image.src =
+      "https://cdn.discordapp.com/emojis/624460839696597015.gif?size=160&quality=lossless";
     input.value = "";
   } else if (input.value < answer) {
     result.textContent = `Try guessing higher! (${input.value})`;
+    image.src =
+      "https://cdn.discordapp.com/emojis/625101593389301780.gif?size=160&quality=lossless";
     input.value = "";
   } else {
     result.textContent = `Congratulations you guessed the correct number! (${answer})`;
+    image.src =
+      "https://cdn.discordapp.com/emojis/643511596034949138.gif?size=160&quality=lossless";
     const btn = document.getElementById("btn");
     btn.textContent = "Try again";
   }
